ca66a48ea5b2945234a0069ecf58156a5d99bf5d
non258 <c0117037bf@edu.teu.ac.jp>
2018-12-14 14:18:28 +0900

    配列の生成にインタを利用

--- inner_product/single/main.cpp
+++ inner_product/single/main.cpp
@@ -4,59 +4,56 @@
 
 int main()
 {
+  // 時間計測開始
+  std::chrono::steady_clock::time_point start,end;
+  start = std::chrono::steady_clock::now();
+
   // random
   std::random_device seed_gen;
   // std::mt19937 engine(seed_gen());
   std::mt19937 engine(100);
   std::uniform_real_distribution<> dist1(0, 10);
 
-  // 時間計測開始
-  std::chrono::system_clock::time_point start,end;
-  start = std::chrono::system_clock::now();
+  long long n = 10000;
 
-  long long n = 1000;
+  int *a; int *b; int *result;
+  a = new int[n*n]; b = new int[n]; result = new int[n];
 
-  double a[n*n],b[n],result[n];
-  // double result_tmp = 0;
+  // int a[n*n], b[n], result[n];
+  
+  int i;
 
-  for (int i = 0; i < n*n; i++)
+  for (i = 0; i < n*n; i++)
   {
     a[i] = dist1(engine);
     // std::cout << a[i] << std::endl;
   }
 
-
-  for (int i = 0; i < n; i++)
+  for (i = 0; i < n; i++)
   {
     b[i] = dist1(engine);
     // std::cout << b[i] << std::endl;
     result[i] = 0;
   }
 
-int i;
-
 #ifdef _OPENMP
   // スレッド数の設定
-  omp_set_num_threads(4);
+  omp_set_num_threads(8);
 #pragma omp parallel for private(i)
 #endif
   for (i = 0; i < n*n; i++)
   {
-    // std::cout << i << i / n << i % n << std::endl;
-    // cout << 0 << 1 / n << endl;
     result[i / n] += a[i] * b[i % n];
-    // std::cout << (int)a[i] << b[i % n] << std::endl;
-    // sleep(1);
+     // sleep(1);
   }
-
-
+  
   // 時間計測終了
-  end = std::chrono::system_clock::now();
-  auto elapsed = std::chrono::duration_cast< std::chrono::nanoseconds>(end - start).count();
+  end = std::chrono::steady_clock::now();
+  auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
   // for (int i = 0; i < n; i++)
   //   std::cout << result[i] << ' ';
   // std::cout << std::endl;
 
-  std::cout << elapsed <<"ns"<< std::endl;
+  std::cout << elapsed <<"ms"<< std::endl;
 }
