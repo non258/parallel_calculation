8537f49c26e81086f4739a761acfe2bf14d0b572
non258 <c0117037bf@edu.teu.ac.jp>
2019-01-05 14:44:52 +0900

    時間計測方法の変更

--- inner_product/single/main.cpp
+++ inner_product/single/main.cpp
@@ -6,10 +6,6 @@
 
 int main()
 {
-  // 時間計測開始
-  std::chrono::steady_clock::time_point start,end;
-  start = std::chrono::steady_clock::now();
-
   // random
   std::random_device seed_gen;
   // std::mt19937 engine(seed_gen());
@@ -17,8 +13,11 @@ int main()
   std::uniform_real_distribution<> dist1(0, 10);
 
   ll n = 1000;
+  int count = 0;
 
   int a[n*n], b[n], result[n];
+
+  std::vector<int> v(100, 0);
   
   ll i;
 
@@ -32,21 +31,32 @@ int main()
     b[i] = dist1(engine);
     result[i] = 0;
   }
+  
+  while (count > 100)
+  {
+    // 時間計測開始
+    std::chrono::steady_clock::time_point start,end;
+    start = std::chrono::steady_clock::now();
 
 #ifdef _OPENMP
-  // スレッド数の設定
-  omp_set_num_threads(8);
+    // スレッド数の設定
+    omp_set_num_threads(8);
 #pragma omp parallel for private(i)
 #endif
-  for (i = 0; i < n*n; i++)
-  {
-    result[i / n] += a[i] * b[i % n];
-     // sleep(1);
-  }
-  
-  // 時間計測終了
-  end = std::chrono::steady_clock::now();
-  auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    for (i = 0; i < n*n; i++)
+    {
+      result[i / n] += a[i] * b[i % n];
+       // sleep(1);
+    }
+    
+    // 時間計測終了
+    end = std::chrono::steady_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
-  std::cout << elapsed <<"ms"<< std::endl;
+    v.push_back(elapsed);
+    count++;
+  }
+
+  int min = *std::max_element(v.begin(), v.end());
+  std::cout << min <<"ms"<< std::endl;
 }
